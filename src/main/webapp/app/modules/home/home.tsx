38bcbee797313c29e7047fafb17c13b88f56808c
Christa <christa@Zipcoders-MBP-6.fios-router.home>
2023-04-29 21:45:50 -0400

    music playing with button press

--- src/main/webapp/app/modules/home/home.tsx
+++ src/main/webapp/app/modules/home/home.tsx
@@ -1,6 +1,7 @@
 import './home.scss';
+import './home.css';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Row, Col, Alert } from 'reactstrap';
@@ -9,77 +10,72 @@ import { useAppSelector } from 'app/config/store';
 
 export const Home = () => {
   const account = useAppSelector(state => state.authentication.account);
+  const [audioPlaying, setAudioPlaying] = useState(false);
+  const audio = new Audio('../../../content/audio/weightless.m4a');
+
+  useEffect(() => {
+    audio.play();
+  }, []);
+
+  const handleAudioButtonClick = () => {
+    if (!audioPlaying) {
+      audio.play();
+      setAudioPlaying(true);
+    } else {
+      audio.pause();
+      setAudioPlaying(false);
+    }
+  };
 
   return (
-    <Row>
-      <Col md="3" className="pad">
+    <Row className="my-custom-class">
+      <Col md="4" className="pad">
         <span className="hipster rounded" />
       </Col>
-      <Col md="9">
-        <h2>Welcome, Java Hipster Hello Christa new!</h2>
-        <p className="lead">This is your homepage</p>
+      <Col md="8">
+        <div className="text-left">
+          <h2 style={{ marginLeft: '0', textAlign: 'left' }}>Panic Support Now</h2>
+          <p className="lead" style={{ marginLeft: '0', textAlign: 'left' }}>
+            Help is here
+          </p>
+        </div>
+
         {account?.login ? (
           <div>
             <Alert color="success">You are logged in as user &quot;{account.login}&quot;.</Alert>
           </div>
         ) : (
-          <div>
-            <Alert color="warning">
-              If you want to
+          <div className="text-right">
+            <Alert color="warning" className="custom-alert custom-alert-large">
+              Have you taken your medication?
               <span>&nbsp;</span>
-              <Link to="/login" className="alert-link">
-                sign in
-              </Link>
-              , you can try the default accounts:
-              <br />- Administrator (login=&quot;admin&quot; and password=&quot;admin&quot;) <br />- User (login=&quot;user&quot; and
-              password=&quot;user&quot;).
-            </Alert>
-
-            <Alert color="warning">
-              You don&apos;t have an account yet?&nbsp;
-              <Link to="/account/register" className="alert-link">
-                Register a new account
-              </Link>
+              <Link to="/login" className="alert-link"></Link>
+              <br style={{ marginBottom: '10px' }} />
+              <br style={{ marginTop: '10px' }} />
+              Do you have a rubberband to snap?
+              <br style={{ marginBottom: '10px' }} />
+              <br />
+              Would you like to call your support person? Press the button below this box.
+              <br style={{ marginBottom: '10px' }} />
+              <br />
+              Press the 'Next prompt' button on the left to walk through a grounding exercise.
+              <br />
+              <div className="button-container" style={{ display: 'flex', alignItems: 'center' }}>
+                {audioPlaying ? (
+                  <button className="audio-button" onClick={handleAudioButtonClick}>
+                    Click to stop audio
+                  </button>
+                ) : (
+                  <button className="audio-button" onClick={handleAudioButtonClick}>
+                    Click to play audio
+                  </button>
+                )}
+                <button className="call-button">Call</button>
+                <button className="next-button btn btn-primary">Next prompt</button>
+              </div>
             </Alert>
           </div>
         )}
-        <p>If you have any question on JHipster:</p>
-
-        <ul>
-          <li>
-            <a href="https://www.jhipster.tech/" target="_blank" rel="noopener noreferrer">
-              JHipster homepage
-            </a>
-          </li>
-          <li>
-            <a href="https://stackoverflow.com/tags/jhipster/info" target="_blank" rel="noopener noreferrer">
-              JHipster on Stack Overflow
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/jhipster/generator-jhipster/issues?state=open" target="_blank" rel="noopener noreferrer">
-              JHipster bug tracker
-            </a>
-          </li>
-          <li>
-            <a href="https://gitter.im/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-              JHipster public chat room
-            </a>
-          </li>
-          <li>
-            <a href="https://twitter.com/jhipster" target="_blank" rel="noopener noreferrer">
-              follow @jhipster on Twitter
-            </a>
-          </li>
-        </ul>
-
-        <p>
-          If you like JHipster, don&apos;t forget to give us a star on{' '}
-          <a href="https://github.com/jhipster/generator-jhipster" target="_blank" rel="noopener noreferrer">
-            GitHub
-          </a>
-          !
-        </p>
       </Col>
     </Row>
   );
